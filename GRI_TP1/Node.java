4ae0508c5f05596d17d5c1d64648f81b89e35442
VincentLIM <vincent.lim@etu.u-paris.fr>
2023-01-06 14:56:01 +0100

    TP1 update

---
+++ GRI_TP1/Node.java
@@ -0,0 +1,51 @@
+class Node {
+    private int id; // Node label in the txt filed remmaped to [0, n[] 
+    private int label; // Node label in the txt filed
+    private int degree; // Number of outgoing edge
+    private int adjListIndex; // Index of the beginning of its outgoing edge list in the adjacent list of all nodes
+    private boolean visited; // true if all of its edge has been examined
+    private int dist; // distance to the considered node
+
+    public Node(int id, int label){
+        this.id = id;
+        this.label = label;
+        this.degree = 0;
+        this.visited = false;
+    }
+
+    public int getID(){
+        return this.id;
+    }
+
+    public int getLabel(){
+        return this.label;
+    }
+
+    public int getDegree(){
+        return this.degree;
+    }
+
+    public void inc_degree(){
+        this.degree++;
+    }
+
+    public int getAdjListIndex(){
+        return this.adjListIndex;
+    }
+
+    public boolean getVisited(){
+        return this.visited;
+    }
+    
+    public void setVisited(){
+        this.visited = true;
+    }
+
+    public int getDist(){
+        return this.dist;
+    }
+    
+    public void setDist(int dist){
+        this.dist = dist;
+    }
+} 
